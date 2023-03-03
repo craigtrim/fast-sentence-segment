9bc7ae85d562152973497525860af12d0b571b85
Craig Trim <craigtrim@gmail.com>
2023-03-02 18:19:05 -0800

    Update Core Dependencies

--- resources/scripts/download-en_core_web_sm.sh
+++ resources/scripts/download-en_core_web_sm.sh
@@ -3,12 +3,12 @@
 # poetry_download_model.sh
 
 # Full path of the file
-DEST="resources/lib/en_core_web_sm-3.3.0.tar.gz"
+DEST="resources/lib/en_core_web_sm-3.5.0.tar.gz"
 
 if [ -f ${DEST} ]; then
     echo "Model $DEST has already been downloaded."
 else
-     SOURCE="https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.3.0/en_core_web_sm-3.3.0.tar.gz"
+     SOURCE="https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.5.0/en_core_web_sm-3.5.0.tar.gz"
      mkdir -p resources/libs
     cd resources/libs
     curl --location $SOURCE --remote-name --output $DEST
